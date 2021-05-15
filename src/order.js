6c7c5752a4dfe8661e166ba13c927a8f9cf288f2
Mae Duphorne <maelduphorne@gmail.com>
2021-05-15 16:01:49 -0600

    complete order exercise

--- src/order.js
+++ src/order.js
@@ -1,7 +1,55 @@
 
+function takeOrder(order, deliveryOrders) {
+  if (deliveryOrders.length < 3){
+  deliveryOrders.push(order);
+}
+};
+
+function refundOrder(orderNumber, deliveryOrders){
+  // for loop
+  for (var i = 0; i < deliveryOrders.length; i++){
+    // if statement
+    if (orderNumber === deliveryOrders[i].orderNumber){
+  // splice method
+      deliveryOrders.splice(i, 1);
+    }
+  }
+}
+
+function listItems(orders){
+//define a variable and assign to an array of order item names
+  var orderItems = []
+  //for loop - intialize, condition, update value
+  for (var i = 0; i < orders.length; i++){
+      //then .push the object to the end of the array
+      orderItems.push(orders[i].item);
+    }
+    //return array as a string - needs spaces
+    return orderItems.join(', ');
+}
+
+function searchOrder(deliveryOrders, order) {
+  // define an additional variable converting object items to an array
+  var orderItems = [];
+  // loop through the array deliveryOrders
+  for (var i = 0; i < deliveryOrders.length; i++){
+    // push item in each object from DeliveryOrders to the new orderItems array
+    orderItems.push(deliveryOrders[i].item);
+  }
+  // if the array includes item,
+  if (orderItems.includes(order)){
+// then return true
+  return true;
+} else {
+  // else, return false
+  return false;
+  }
+}
+
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+   takeOrder,
+   refundOrder,
+   listItems,
+   searchOrder
 }
