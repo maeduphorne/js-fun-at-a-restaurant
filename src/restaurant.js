1935a7622f9a422d2153f2c09ad1cff226f1b0f1
Mae Duphorne <maelduphorne@gmail.com>
2021-05-16 15:49:37 -0600

    Complete restaurant.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,51 @@
 
+function createRestaurant(restaurantName) {
+  var restaurant = {
+    name: restaurantName,
+    menus: {
+      breakfast:[],
+      lunch:[],
+      dinner:[],
+    },
+
+  }
+  return restaurant;
+};
+
+
+
+function addMenuItem(restaurant, menuItem) {
+  if (menuItem.type === 'breakfast' && !restaurant.menus.breakfast.includes(menuItem)) {
+    restaurant.menus.breakfast.push(menuItem);
+  } else if (menuItem.type === 'lunch'&& !restaurant.menus.lunch.includes(menuItem)){
+    restaurant.menus.lunch.push(menuItem);
+  } else if (menuItem.type === 'dinner' && !restaurant.menus.dinner.includes(menuItem)){
+    restaurant.menus.dinner.push(menuItem);
+  }
+
+};
+
+
+function removeMenuItem(restaurant, nameRemove, typeRemove) {
+var menuType = restaurant.menus[typeRemove]
+if (menuType.length > 0){
+
+for (var i = 0; i < menuType.length; i++){
+  if (menuType[i].name === nameRemove) {
+    menuType.splice(menuType[i], 1);
+    return `No one is eating our ${nameRemove} - it has been removed from the ${typeRemove} menu!`
+ }
+}
+
+} else {
+  return `Sorry, we don't sell ${nameRemove}, try adding a new recipe!`
+}
+};
+
+
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
