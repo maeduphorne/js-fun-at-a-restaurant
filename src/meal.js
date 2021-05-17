b4831a084322223fdbfe3dd24eac335c7c93cc77
Mae Duphorne <maelduphorne@gmail.com>
2021-05-16 20:09:38 -0600

    Update meal.js file

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,46 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
+};
 
+function createMenuItem(menuItemName, price, type){
+  var menuItem = {
+    name: menuItemName,
+    price: price,
+    type: type
+  }
+  return menuItem
+};
+
+function addIngredients(ingredient, ingredients){
+  if (!ingredients.includes(ingredient)) {
+  return ingredients.push(ingredient);
+}
+};
+
+function formatPrice(initialPrice){
+  return `$${initialPrice}`;
+};
+
+function decreasePrice(price){
+  var discount = (price * .10);
+  var decreasedPrice = (price - discount);
+  return decreasedPrice
 }
 
+function createRecipe(title, ingredients, menuItemType){
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+  return recipe
+};
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
